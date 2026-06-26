acb1903a1c272101bc788c25905b63f61ba9da0b
Amna Ali <amnaali050606@gmail.com>
2026-06-26 15:55:57 +0500

    Solve <making faces.py>

---
+++ making faces.py
@@ -0,0 +1,8 @@
+def convert(text):
+    text = text.replace(":)", "🙂")
+    text = text.replace(":(", "🙁")
+    return text
+def main():
+    user_input = input("Enter text: ")
+    print(convert(user_input))
+main()
