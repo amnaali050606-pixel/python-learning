59d7057bca5fc08b76808e2b326ee2d5fe65d6c3
Amna Ali <amnaali050606@gmail.com>
2026-06-29 23:20:25 +0500

    nutrition.py

---
+++ nutrition.py
@@ -0,0 +1,19 @@
+fruits = input(" enter the name of fruit ").lower()
+if fruits == "lime":
+    print("calories:20")
+elif fruits == "avocado" or fruits == "cantaloupe" or fruits=="honeydew melon" or fruits=="pineapple" or fruits=="strawberries" or fruits=="tangerine":
+    print(" calories: 50 ")
+elif fruits == "grapefruit" or fruits=="nectarine" or fruits=="peach":
+    print(" calories: 60")
+elif fruits == "plums":
+    print("calories:70")
+elif fruits == "orange" or fruits=="watermelon":
+    print("calories: 80")
+elif fruits == "grapes" or fruits=="kiwi":
+    print("calories:90")
+elif fruits == "pear" or fruits=="sweet cherries":
+    print("calories:100")
+elif fruits == "banana":
+    print("calories: 110")
+elif fruits == "apple":
+    print("calories:130")
