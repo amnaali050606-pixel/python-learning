04193c8954d5434b070dc28ab548b4338650d823
Amna Ali <amnaali050606@gmail.com>
2026-07-01 18:17:32 +0500

    fuel.py

---
+++ fuel.py
@@ -0,0 +1,27 @@
+def main():
+    while True:
+        try:
+            fraction = input("Fraction: ")
+            x, y = fraction.split("/")
+
+            x = int(x)
+            y = int(y)
+
+            if x > y:
+                raise ValueError
+
+            percentage = round((x / y) * 100)
+
+            if percentage <= 1:
+                print("E")
+            elif percentage >= 99:
+                print("F")
+            else:
+                print(f"{percentage}%")
+            break
+
+        except (ValueError, ZeroDivisionError):
+            pass
+
+
+main()
